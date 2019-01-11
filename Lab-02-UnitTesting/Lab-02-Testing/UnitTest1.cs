2c02039a383b7ffcc708b81b399e7035abb98eb1
LutherMckeiver <leoluther96@gmail.com>
2019-01-11 11:17:16 -0800

    finished tests and refactored methods

--- Lab-02-UnitTesting/Lab-02-Testing/UnitTest1.cs
+++ Lab-02-UnitTesting/Lab-02-Testing/UnitTest1.cs
@@ -11,18 +11,34 @@ namespace Lab_02_Testing
         {
             Assert.Equal(1000, Program.ViewBalance(1000));
         }
-      /*
-        [Fact]
-        public void CanWithdrawMoney()
+
+       
+        [Theory]
+        [InlineData(500, "200", "You have 300 left in your account.")]
+        public void CanWithdrawMoney(decimal userBalance, string userAmount, string expectation)
         {
-            Assert.Equal(200, Program.WithdrawMoney(1000));
+            Assert.Equal(expectation, Program.WithdrawMoney(userBalance, userAmount));
         }
 
-        [Fact]
-        public void CanDepositMoney()
+        [Theory]
+        [InlineData(500, "1000", "Funds insuffiecient, you have 500 in your account.")]
+        public void CantWithdrawMoreThanBalance(decimal userBalance, string userAmount, string expectation)
         {
-            Assert.Equal(500, Program.DepositMoney(1000));
+            Assert.Equal(expectation, Program.WithdrawMoney(userBalance, userAmount));
         }
-        */
+
+        [Theory]
+        [InlineData(1000, "1000", "1000.00" )]
+        public void CantWithdrawEqualToBalance(decimal userBalance, string userAmount, string expectation)
+        {
+            Assert.Equal(expectation, Program.WithdrawMoney(userBalance, userAmount));
+        }
+        
+        [Theory]
+        [InlineData(500, "1500", "2000")]
+        public void CanDepositMoney(decimal userBalance, string userInput, string expectation)
+        {
+            Assert.Equal(expectation, Program.DepositMoney(userBalance, userInput));
+        }
     }
 }
--- Lab-02-UnitTesting/Lab-02-UnitTesting/Program.cs
+++ Lab-02-UnitTesting/Lab-02-UnitTesting/Program.cs
@@ -6,60 +6,17 @@ namespace Lab_02_UnitTesting
     public class Program
     {
 
+        public static decimal userBalance = 500.00M;
 
         static void Main(string[] args)
         {
-            PrintWelcomeMessage();
-            BankOptions();
-            
-            while (true)
-            {
-                decimal userBalance = 1000.00M;
-                Console.Write("> ");
-                string userInput = Console.ReadLine();
-
-                if (userInput == "quit")
-                {
-                    Console.WriteLine("Have a good day!");
-                    Environment.Exit(0);
-                }
-                else if (userInput == "1")
-                {
-                    ViewBalance(userBalance);
-                    continue;
-                }
-                else if (userInput == "2")
-                {
-                    WithdrawMoney(userBalance);
-                    continue;
-                }
-                else if (userInput == "3")
-                {
-                    DepositMoney(userBalance);
-                    continue;
-                }
-                else
-                {
-                    Console.WriteLine("Please choose a valid number \n");
-                    BankOptions();
-                    continue;
-                }
-                Console.ReadLine();
-            }
-        }
-
-        static void PrintWelcomeMessage()
-        {
             Console.WriteLine("*************************************************");
-            Console.WriteLine("***        Welcome To Luther's Bank!          ***");
+            Console.WriteLine("***        Welcome To Luther's ATM!           ***");
             Console.WriteLine("***     Please see your options below!        ***");
             Console.WriteLine("***                                           ***");
             Console.WriteLine("***     To quit at any time, type quit        ***");
             Console.WriteLine("************************************************* \n");
-        }
 
-        static void BankOptions()
-        {
             Console.WriteLine("**************************************************");
             Console.WriteLine("***    What would you like to do today?        ***");
             Console.WriteLine("***    1. View Balance                         ***");
@@ -68,15 +25,79 @@ namespace Lab_02_UnitTesting
             Console.WriteLine("***                                            ***");
             Console.WriteLine("***    Choose a number to continue.            ***");
             Console.WriteLine("**************************************************");
+            Console.Write("> ");
 
+            string userInput = Console.ReadLine();
+            ATMOptions(userInput);
+            
+            while (Continue() == "c")
+            {
+                Console.WriteLine("**************************************************");
+                Console.WriteLine("***    What would you like to do today?        ***");
+                Console.WriteLine("***    1. View Balance                         ***");
+                Console.WriteLine("***    2. Withdraw Money                       ***");
+                Console.WriteLine("***    3. Deposit Money                        ***");
+                Console.WriteLine("***                                            ***");
+                Console.WriteLine("***    Choose a number to continue.            ***");
+                Console.WriteLine("**************************************************");
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                ATMOptions(input);
+            } 
         }
+
+              
+        public static void ATMOptions(string userInput)
+        {
+            switch (userInput)
+            {
+                case "quit":
+                    Console.WriteLine("Have a good day!");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                    break;
+                case "1":
+                    ViewBalance(userBalance);
+                    Console.ReadLine();
+                    break;
+                case "2":
+                    Console.WriteLine("How much would you like to withdraw?");
+                    Console.Write("> ");
+                    string userAmount = Console.ReadLine();
+                    Console.WriteLine(WithdrawMoney(userBalance, userAmount));
+                    Console.ReadLine();
+                    break;
+                case "3":
+                    Console.WriteLine("How much would you like to deposit?");
+                    Console.Write("> ");
+                    string userAnswer = Console.ReadLine();
+                    Console.WriteLine(DepositMoney(userBalance, userAnswer));
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("Pleae choose a valid number \n");
+                    string input = Console.ReadLine();
+                    ATMOptions(input);
+                    Console.ReadLine();
+                    break;
+            }
+        }
+        
+        public static string Continue()
+        {
+            Console.WriteLine("Do you want to continue, or quit?");
+            Console.WriteLine("Press any other key to exit, or type c to continue");
+            Console.Write("> ");
+            string userAnswer = Console.ReadLine();
+            
+            return userAnswer;
+        }
+
 
         public static decimal ViewBalance(decimal userBalance)
         {
-
             string formatCurrency = String.Format("{0:C0}", Convert.ToInt32(userBalance));
             Console.WriteLine("Your balance is: {0}", formatCurrency);
-
             if (userBalance < 10)
             {
                 Console.WriteLine("Your balance is lower than $10, you should deposit some money!");
@@ -85,69 +106,59 @@ namespace Lab_02_UnitTesting
             return userBalance;
         }
 
-        public static decimal WithdrawMoney(decimal userBalance)
-        {
-            Console.WriteLine("How much would you like to withdraw?");
-            Console.Write("> ");
 
-            string userAmount = Console.ReadLine();
+        public static string WithdrawMoney(decimal userBalance, string userAmount)
+        {
             try
             {
                 decimal correctNumber = decimal.Parse(userAmount);
 
                 if (correctNumber > userBalance)
                 {
-                    Console.WriteLine("Funds insufficient");
-                    Console.WriteLine("Your balance is: {0}", userBalance);
-                    return userBalance;
+                   return "Funds insuffiecient, you have " + userBalance.ToString() + " in your account.";
                 }
                 else if (correctNumber < userBalance)
                 {
-                    userBalance = userBalance - correctNumber;
+                    Program.userBalance = userBalance - correctNumber;
                     Console.WriteLine("You have succesfully taken out: {0}", correctNumber);
-                    Console.WriteLine("You have {0} left in your account", userBalance);
-                    return userBalance;
+                    return "You have " + Program.userBalance.ToString() + " left in your account.";
 
                 }
                 else if (correctNumber == userBalance)
                 {
                     Console.WriteLine("Error: The amount you are trying to withdraw is the same as your balance.");
-                    Console.WriteLine("Your current balance is {0}", userBalance);
-                    return userBalance;
+                    Console.WriteLine("Your current balance is {0}", Program.userBalance);
+                    return Program.userBalance.ToString(); 
                 }
-
-
             }
             catch (FormatException e)
             {
                 Console.WriteLine("{0} Not a valid format.", e);
                 throw;
-
             }
-            return userBalance;
+            return Program.userBalance.ToString();
         }
 
-        public static decimal DepositMoney(decimal userBalance)
-        {
-            Console.WriteLine("How much would you like to deposit?");
-            Console.Write("> ");
-            string userInput = Console.ReadLine();
 
+        public static string DepositMoney(decimal userBalance, string userInput)
+        {
             try
             {
                 decimal formattedResponse = decimal.Parse(userInput);
 
-                userBalance = formattedResponse + userBalance;
-                Console.WriteLine("Your new balance is {0} ", userBalance);
+                Program.userBalance = formattedResponse + userBalance;
+                Console.WriteLine("Your new balance is {0} ", Program.userBalance);
             }
             catch (FormatException e)
             {
                 Console.WriteLine("{0} Not a valid format", e);
                 throw;
             }
-            return userBalance;
+            return Program.userBalance.ToString();
         } 
     } 
 }
+
+
 
 
